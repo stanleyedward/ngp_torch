fe79f329866acf32bc8bd95e6d5e48399d9075d1
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2024-11-17 16:40:42 +0530

    finish forward train vol ren kernel

--- src/volumerenderer.cu
+++ src/volumerenderer.cu
@@ -19,6 +19,7 @@ __global__ void composite_train_fw_kernel(
     const int ray_idx = rays_a[n][0]; 
     const int start_idx = rays_a[n][1];
     const int N_samples = rays_a[n][2];
+
     //no hits occured
     if(N_samples==0 || start_idx+N_samples>=sigmas.size(0)){ 
         rgb[ray_idx][0] = rgb_bg[0];
@@ -27,7 +28,35 @@ __global__ void composite_train_fw_kernel(
         return;
     }
     // front to back compositing
+    int samples = 0;
+    scalar_t T = 1.0f;
+    scalar_t r = 0.0f;
+    scalar_t g = 0.0f;
+    scalar_t b = 0.0f;
+    scalar_t op = 0.0f;
+    scalar_t d = 0.0f;
 
+    while (samples < N_samples){
+        const int s = start_idx + samples;
+        const scalar_t a = 1.0f - __expf(-sigmas[s]*deltas[s]);
+        const scalar_t w = a * T;
+
+        r += w*rgbs[s][0];
+        g += w*rgbs[s][1];
+        b += w*rgbs[s][2];
+        d += w*ts[s];
+        op+= w;
+        T *= 1.0f-a;
+
+        if (T<=T_threshold) break; //ray has enough opacity
+        samples ++;
+    }
+
+    rgb[ray_idx][0] = r + rgb_bg[0]*(1-op);
+    rgb[ray_idx][1] = g + rgb_bg[1]*(1-op);
+    rgb[ray_idx][2] = b + rgb_bg[2]*(1-op);
+    opacity[ray_idx] = op;
+    depth[ray_idx] = d;
 }
 std::vector<torch::Tensor> composite_train_fw_cu(
     const torch::Tensor sigmas,
